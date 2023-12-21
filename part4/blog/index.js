a6c1a91d11b80763d6ad076e953b21098c3374b9
burdas <burdasparmarcos@gmail.com>
2023-12-21 15:54:32 +0100

    Part4 refactorin blog proyect

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -2,31 +2,16 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
 const config = require('./utils/config')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blog')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
